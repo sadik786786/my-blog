bfdc0ee319f9f4a1569052675bab0d880d9eb61c
epsilon <ansarisadik2006@gmail.com>
2025-12-16 12:07:15 +0530

    Update image domains to remotePatterns in config

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['lh3.googleusercontent.com','res.cloudinary.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+    ],
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
   },
-  output:'export'
 };
 
 export default nextConfig;
