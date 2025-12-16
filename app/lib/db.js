d9103bb3e2f2a2b3ea106459acfbae0698262207
epsilon <ansarisadik2006@gmail.com>
2025-12-16 12:04:57 +0530

    Use environment variables for database configuration

--- app/lib/db.js
+++ app/lib/db.js
@@ -1,10 +1,10 @@
 import mysql from "mysql2/promise";
 
 const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "my_blog",
+  host: process.env.host,
+  user: process.env.user,
+  password: process.env.password,
+  database: process.env.database,
 });
 
 export default db;
