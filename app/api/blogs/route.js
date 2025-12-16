c548d2ef5670ad19e262a2588be19750df5e71ef
Sadik Ansari <ansarisadik2006@gmail.com>
2025-12-16 21:09:35 +0530

    Merge remote changes

--- app/api/blogs/route.js
+++ app/api/blogs/route.js
@@ -2,7 +2,7 @@ import db from "@/app/lib/db";
 
 export async function GET(req, { params }) {
   try {
-    const [rows] = await db.query("SELECT * FROM posts");
+    const rows = await db.query("SELECT * FROM posts");
     return new Response(
       JSON.stringify({ success: true, data: rows }), 
       { status: 200 }
